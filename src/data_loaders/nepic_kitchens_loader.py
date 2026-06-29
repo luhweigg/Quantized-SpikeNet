b28c9c93c3667fb34c8dd62d7b7063fc7764ae5b
Maxime Frys <maxime.frys.etu@univ-lille.fr>
2026-06-29 11:34:02 +0200

    review of the full nepic dataloader

--- src/data_loaders/nepic_kitchens_loader.py
+++ src/data_loaders/nepic_kitchens_loader.py
@@ -1,48 +1,72 @@
+import os
 import torch
-import tonic
-import tonic.transforms as transforms
-from torch.utils.data import DataLoader
+import numpy as np
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+
+
+class NEPICKitchens(Dataset):
+    """
+    Dataset loader for N-EPIC Kitchens pre-processed voxel grids.
+    """
+
+    def __init__(self, root_dir: str, split: str = "train", data_type: str = "voxels_xy_3"):
+        self.root_dir = root_dir
+        self.samples = []
+        
+        domains = ["D1", "D2", "D3"]
+        
+        for domain in domains:
+            pkl_path = os.path.join(root_dir, f"{domain}_{split}.pkl")
+            if not os.path.exists(pkl_path):
+                continue
+                
+            df = pd.read_pickle(pkl_path)
+            
+            for idx, row in df.iterrows():
+                video_id = row.get("video_id", "")
+                verb_class = row.get("verb_class", 0)
+                
+                uid = row.get("uid", idx)
+                file_name = f"event_{int(uid):010d}.npy"
+                file_path = os.path.join(root_dir, data_type, video_id, file_name)
+                
+                if os.path.exists(file_path):
+                    self.samples.append((file_path, verb_class))
+
+    def __len__(self) -> int:
+        return len(self.samples)
+
+    def __getitem__(self, index: int):
+        file_path, label = self.samples[index]
+        data = np.load(file_path)
+        tensor_data = torch.from_numpy(data).float()
+        
+        if tensor_data.ndim == 4 and tensor_data.shape[0] == 2:
+            tensor_data = tensor_data.permute(1, 0, 2, 3)
+        elif tensor_data.ndim == 3:
+            tensor_data = tensor_data.unsqueeze(1)
+            
+        return tensor_data, label
+
 
 def custom_collate_fn(batch):
-    events, targets = torch.utils.data.default_collate(batch)
-    events = events.transpose(0, 1)
-    return events, targets
+    data, targets = torch.utils.data.default_collate(batch)
+    data = data.transpose(0, 1)
+    return data, targets
 
-def get_nepic_kitchens_loaders(batch_size=32, n_time_bins=15, num_workers=4):
+
+def get_nepic_kitchens_loaders(batch_size: int = 16, n_time_bins: int = 15, num_workers: int = 4):
     """
-    Data loader for N-EPIC Kitchens with 8 action classes.
-    Assumes tonic.datasets.NEPICKitchens exists or has been custom implemented.
+    Initializes and returns train and test DataLoaders for N-EPIC Kitchens.
     """
-    sensor_size = (346, 260, 2)
+    data_dir = "./data/N-EPIC-Kitchens"
     
-    train_set = tonic.datasets.NEPICKitchens(save_to="./data", train=True)
-    test_set = tonic.datasets.NEPICKitchens(save_to="./data", train=False)
-
-    train_transform = transforms.Compose(
-        [
-            transforms.SpatialJitter(sensor_size=sensor_size, clip_outliers=True),
-            transforms.ToFrame(sensor_size=sensor_size, n_time_bins=n_time_bins),
-        ]
-    )
-
-    test_transform = transforms.Compose(
-        [transforms.ToFrame(sensor_size=sensor_size, n_time_bins=n_time_bins)]
-    )
-
-    cached_train = tonic.DiskCachedDataset(
-        train_set,
-        cache_path="./data/cache/nepic_kitchens/raw_train",
-        transform=train_transform,
-    )
-    
-    cached_test = tonic.DiskCachedDataset(
-        test_set,
-        cache_path="./data/cache/nepic_kitchens/raw_test",
-        transform=test_transform,
-    )
+    train_set = NEPICKitchens(root_dir=data_dir, split="train")
+    test_set = NEPICKitchens(root_dir=data_dir, split="test")
 
     train_loader = DataLoader(
-        cached_train,
+        train_set,
         batch_size=batch_size,
         shuffle=True,
         collate_fn=custom_collate_fn,
@@ -51,7 +75,7 @@ def get_nepic_kitchens_loaders(batch_size=32, n_time_bins=15, num_workers=4):
     )
 
     test_loader = DataLoader(
-        cached_test,
+        test_set,
         batch_size=batch_size,
         shuffle=False,
         collate_fn=custom_collate_fn,
