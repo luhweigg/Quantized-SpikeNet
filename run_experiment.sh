c0f4d85c26573aba60280f9ecd5b881a9fa0eefc
Maxime Frys <maxime.frys.etu@univ-lille.fr>
2026-07-29 17:04:40 +0200

    feat: add run_experiment script for automated model training across datasets

---
+++ run_experiment.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+EPOCHS=20
+THRESHOLDS=(0.5 1.0 1.5)
+
+run_experiment() {
+local dataset=$1
+local arch=$2
+local time=$3
+local batch=$4
+local vth=$5
+
+python main.py \
+    --dataset $dataset \
+    --architecture $arch \
+    --epochs $EPOCHS \
+    --batch_size $batch \
+    --Time $time \
+    --v_threshold $vth \
+    --use_wandb
+
+sleep 5
+
+
+}
+
+for t in 2 4 8 16 32; do
+for vth in "${THRESHOLDS[@]}"; do
+run_experiment "nmnist" "SpikingMLP" $t 64$vth
+done
+done
+
+for t in 2 4 8 16 32; do
+for vth in "${THRESHOLDS[@]}"; do
+run_experiment "cifar10" "SpikingVGG4" $t 16$vth
+done
+done
+
+for t in 2 4 8 16 32; do
+for vth in "${THRESHOLDS[@]}"; do
+run_experiment "dvs_gesture" "SpikingVGG5" $t 16$vth
+done
+done
+
+for t in 2 4 8; do
+for vth in "${THRESHOLDS[@]}"; do
+run_experiment "nepic_kitchens" "SpikingVGG11" $t 4$vth
+done
+done
