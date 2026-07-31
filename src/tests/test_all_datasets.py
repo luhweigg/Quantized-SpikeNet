d229462ff6619bb7fc2206d83a60f77dcf460f55
Maxime Frys <maxime.frys.etu@univ-lille.fr>
2026-07-31 11:28:09 +0200

    refactor: comment out unused datasets in test_all_datasets.py

--- src/tests/test_all_datasets.py
+++ src/tests/test_all_datasets.py
@@ -2,14 +2,14 @@ import subprocess
 import sys
 
 DATASETS = [
-    "nmnist",
-    "cifar10",
-    "dvs_gesture",
+    #"nmnist",
+    #"cifar10",
+    #"dvs_gesture",
     "nepic_kitchens",
     "ncaltech101",
-    "shd",
-    "dvs_lip",
-    "ssc",
+    #"shd",
+    #"dvs_lip",
+    #"ssc",
 ]
 
 
