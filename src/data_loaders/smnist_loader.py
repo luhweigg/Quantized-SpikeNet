e4ff03e3499f3454fa37767aa40e1a3fa72b16a1
Maxime Frys <maxime.frys.etu@univ-lille.fr>
2026-07-31 12:44:47 +0200

    refactor: update SMNIST configuration and loader to use Spiking1DCNN architecture

--- src/data_loaders/smnist_loader.py
+++ src/data_loaders/smnist_loader.py
@@ -1,23 +1,19 @@
 import torch
 import tonic
 import tonic.transforms as transforms
-import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
-def custom_collate_fn(batch):
+def smnist_collate_fn(batch):
     targets = torch.tensor([b[1] for b in batch], dtype=torch.long)
-    events_list = []
-    for b in batch:
-        ev = torch.as_tensor(b[0]).float()
-        ev = F.interpolate(ev, size=(128, 128), mode='bilinear', align_corners=False)
-        events_list.append(ev)
-        
+    events_list = [torch.as_tensor(b[0]).float() for b in batch]
+    
     events = torch.stack(events_list)
     events = events.transpose(0, 1)
     return events, targets
 
 def get_smnist_loaders(batch_size: int, time_steps: int, num_workers: int = 4):
-    sensor_size = (28, 28, 2)
+    sensor_size = (99, 2)
+    
     transform = transforms.Compose([
         transforms.ToFrame(sensor_size=sensor_size, n_time_bins=time_steps),
     ])
@@ -25,6 +21,7 @@ def get_smnist_loaders(batch_size: int, time_steps: int, num_workers: int = 4):
     train_set = tonic.datasets.SMNIST(save_to="./data", train=True, transform=transform)
     test_set = tonic.datasets.SMNIST(save_to="./data", train=False, transform=transform)
 
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, collate_fn=custom_collate_fn, num_workers=num_workers, pin_memory=True)
-    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, collate_fn=custom_collate_fn, num_workers=num_workers, pin_memory=True)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, collate_fn=smnist_collate_fn, num_workers=num_workers, pin_memory=True)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, collate_fn=smnist_collate_fn, num_workers=num_workers, pin_memory=True)
+    
     return train_loader, test_loader
