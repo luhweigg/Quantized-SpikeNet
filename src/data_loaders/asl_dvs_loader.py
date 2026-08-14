dd6b793f0cdebe4c752dac305fcebdbf367d7646
Maxime Frys <maxime.frys.etu@univ-lille.fr>
2026-08-14 10:44:48 +0200

    refactor: update collate functions and data loader configurations for ASL DVS and NAV Gesture datasets

--- src/data_loaders/asl_dvs_loader.py
+++ src/data_loaders/asl_dvs_loader.py
@@ -4,17 +4,21 @@ from torch.utils.data import DataLoader, random_split
 from spikingjelly.datasets.asl_dvs import ASLDVS
 
 def custom_collate_fn_asl(batch):
-    events, targets = torch.utils.data.default_collate(batch)
-    events = events.transpose(0, 1)
-    T, B, C, H, W = events.shape
-    events = events.reshape(T * B, C, H, W)
-    events = F.interpolate(events.float(), size=(64, 64), mode='bilinear', align_corners=False)
-    events = events.view(T, B, C, 64, 64)
+    events_list, targets = [], []
+    for x, y in batch:
+        if not isinstance(x, torch.Tensor):
+            x = torch.tensor(x)
+        x = F.interpolate(x.float(), size=(64, 64), mode='bilinear', align_corners=False)
+        events_list.append(x)
+        targets.append(y)
     
+    events = torch.stack(events_list)
+    events = events.transpose(0, 1)
+    targets = torch.tensor(targets, dtype=torch.long)
     return events, targets
 
 def get_asl_dvs_loaders(batch_size: int, time_steps: int, num_workers: int = 4, split_seed: int = 42):
-    dataset = ASLDVS(root="./data/ASL-DVS", data_type='frame', frames_num=time_steps)
+    dataset = ASLDVS(root="./data/ASL-DVS", data_type='frame', frames_number=time_steps, split_by='number')
     
     train_size = int(0.8 * len(dataset))
     test_size = len(dataset) - train_size
@@ -23,5 +27,4 @@ def get_asl_dvs_loaders(batch_size: int, time_steps: int, num_workers: int = 4, 
 
     train_loader = DataLoader(train_subset, batch_size=batch_size, shuffle=True, collate_fn=custom_collate_fn_asl, num_workers=num_workers, pin_memory=True)
     test_loader = DataLoader(test_subset, batch_size=batch_size, shuffle=False, collate_fn=custom_collate_fn_asl, num_workers=num_workers, pin_memory=True)
-    
     return train_loader, test_loader
--- src/data_loaders/nav_gesture_loader.py
+++ src/data_loaders/nav_gesture_loader.py
@@ -1,17 +1,30 @@
 import torch
-from torch.utils.data import DataLoader
-from spikingjelly.datasets.nav_gesture import NavGesture
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, random_split
+from spikingjelly.datasets.nav_gesture import NAVGestureWalk
 
 def custom_collate_fn_nav(batch):
-    events, targets = torch.utils.data.default_collate(batch)
-    events = events.transpose(0, 1)
-    return events.float(), targets
-
-def get_nav_gesture_loaders(batch_size: int, time_steps: int, num_workers: int = 4):
-    train_set = NavGesture(root="./data/NavGesture", train=True, data_type='frame', frames_num=time_steps)
-    test_set = NavGesture(root="./data/NavGesture", train=False, data_type='frame', frames_num=time_steps)
-
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, collate_fn=custom_collate_fn_nav, num_workers=num_workers, pin_memory=True)
-    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, collate_fn=custom_collate_fn_nav, num_workers=num_workers, pin_memory=True)
+    events_list, targets = [], []
+    for x, y in batch:
+        if not isinstance(x, torch.Tensor):
+            x = torch.tensor(x)
+        x = F.interpolate(x.float(), size=(64, 64), mode='bilinear', align_corners=False)
+        events_list.append(x)
+        targets.append(y)
     
+    events = torch.stack(events_list)
+    events = events.transpose(0, 1) 
+    targets = torch.tensor(targets, dtype=torch.long)
+    return events, targets
+
+def get_nav_gesture_loaders(batch_size: int, time_steps: int, num_workers: int = 4, split_seed: int = 42):
+    dataset = NAVGestureWalk(root="./data/NavGesture", data_type='frame', frames_number=time_steps, split_by='number')
+    
+    train_size = int(0.8 * len(dataset))
+    test_size = len(dataset) - train_size
+    generator = torch.Generator().manual_seed(split_seed)
+    train_subset, test_subset = random_split(dataset, [train_size, test_size], generator=generator)
+
+    train_loader = DataLoader(train_subset, batch_size=batch_size, shuffle=True, collate_fn=custom_collate_fn_nav, num_workers=num_workers, pin_memory=True)
+    test_loader = DataLoader(test_subset, batch_size=batch_size, shuffle=False, collate_fn=custom_collate_fn_nav, num_workers=num_workers, pin_memory=True)
     return train_loader, test_loader
